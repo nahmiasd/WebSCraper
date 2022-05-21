6eab9987f60df0a645a73f743e6fc15177e666ea
Daniel Nahmias <nahmiasd@post.bgu.ac.il>
2022-05-21 19:15:12 +0300

    added test_generate_graph_from_urls

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -1,8 +1,9 @@
-from src.utils import get_emails_from_website_text, get_links_from_website_text
+from src.utils import get_emails_from_website_text, get_links_from_website_text, generate_graph_from_scraped_urls
 import pytest
 from pytest_lazyfixture import lazy_fixture
 import codecs
 from pathlib import Path
+from src.urlinfo import URLInfo
 
 dummy_web_pages_dir = Path(__file__).parent.joinpath('dummy_web_pages')
 
@@ -53,3 +54,15 @@ def test_get_emails_from_website_test(page, page_name, expected_email_per_page):
 def test_get_links_from_website_text(page, page_name, expected_links):
     links = get_links_from_website_text(page)
     assert links == expected_links[page_name]
+
+
+def test_generate_graph_from_urls(page_a, page_b, page_c, expected_links):
+    urls_infos = []
+    for page, page_name in zip([page_a, page_b, page_c], ['page a', 'page b', 'page c']):
+        emails = get_emails_from_website_text(page)
+        links = get_links_from_website_text(page)
+        url_info = URLInfo(page_name, 'domain', emails, links)
+        urls_infos.append(url_info)
+    G = generate_graph_from_scraped_urls(urls_infos)
+    for u, v in G.edges():
+        assert v in expected_links[u]
